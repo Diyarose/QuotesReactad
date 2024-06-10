186177c01b1ddc658194c92758f07bcb038fa30f
Diyarose <dhiyarosedavis@gmail.com>
2024-06-10 06:45:29 +0530

    quotes are updated

--- src/components/AddQuotes.jsx
+++ src/components/AddQuotes.jsx
@@ -30,7 +30,7 @@ const AddQuotes = () => {
                             <label htmlFor="" className="form-label">Quote</label>
                             <textarea name="" id="" className="form-control" name='quote' value={data.quote} onChange={inputHandler}></textarea>
                         </div>
-                        <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">Author</label>
                             <input type="text" className="form-control" name='author' value={data.author} onChange={inputHandler} />
                         </div>
